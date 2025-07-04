6ae6fa7ae18f5efaefe010aa327e5140b5cde232
karina146 <karina14065@gmail.com>
2025-07-05 00:51:38 +0200

    .

--- eshop/main/urls.py
+++ eshop/main/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = 'main'
 
 urlpatterns = [
-     path('', views.index_list, name='index_list'),
+     path('', views.product_list, name='product_list'),
+     path('/category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
      path('<slug:slug>/', views.product_details,
           name='product_detail')
 ]
