78d2dfa57c3e12ed517e8daee6d6c4ffabed9d5b
karina146 <karina14065@gmail.com>
2025-07-04 23:58:05 +0200

    добавила views

--- eshop/main/views.py
+++ eshop/main/views.py
@@ -1,1 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+
+def product_details(request, slug):
+    product = get_object_or_404(Product, slug=slug, available=True)
+
+    return render(request, 'main/product/detail.html', {'product':product})
+
+
+
+# def index_list(request, slug):
+#     product = get_object_or_404(Product, slug=slug, available=True)
+
+#     return render(request, 'main/index/index.html', {'product':product})
