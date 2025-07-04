028478b7928301644c1925a4106cbfe18892b7ba
karina146 <karina14065@gmail.com>
2025-07-05 00:38:15 +0200

    добавила product_list

--- eshop/main/views.py
+++ eshop/main/views.py
@@ -8,8 +8,15 @@ def product_details(request, slug):
     return render(request, 'main/product/detail.html', {'product':product})
 
 
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
 
-def index_list(request):
-    category = Category.objects.filter()
-
-    return render(request, 'main/index/index.html', {'category':category})
+    return render(request, 'main/product/list.html',
+                  {'category':category,
+                   'categories':categories,
+                   'products':products})
