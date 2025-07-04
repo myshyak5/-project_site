d17bdc13356543297caba697afb721877141ed60
karina146 <karina14065@gmail.com>
2025-07-04 23:02:03 +0200

    добавила основу для моделей и admin

--- eshop/main/admin.py
+++ eshop/main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price',
+                    'gender', 'size', 'available',
+                    'created', 'updated', 'discount']
+    list_filter = ['available', 'gender', 'size']
+    list_editable = ['price', 'available', 'discount']
+    prepopulated_fields = {'slug': ('name',)}
--- eshop/main/models.py
+++ eshop/main/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Category(models.Model):
+    name  = models.CharField(max_length=20, unique=True)
+    slug = models.SlugField(max_length=20, unique=True)
+
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['name'])]
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products',
+                                 on_delete=models.PROTECT)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    GENDER = [('female', 'female'), ('male', 'male'), ('child', 'child')]
+    gender = models.CharField(max_length=10, choices=GENDER, default='female')
+    size = models.IntegerField(max_length=10, default=30)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    discount = models.DecimalField(default=0.00, max_digits=5,
+                                   decimal_places=2)
+    
+
+    class Meta:
+        ordering = ['name']
+        indexes = [models.Index(fields=['id', 'slug']),
+                   models.Index(fields=['name']),
+                   models.Index(fields=['-created'])]
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('main:product_detail', args=[self.slug])
+    
+    def sell_price(self):
+        if self.discount:
+            return round(self.price-self.price*self.discount/100, 2)
+        return self.price
+    
