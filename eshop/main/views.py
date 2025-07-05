022ecd474319b7af3f2fc43bdd1456ede944c4f0
StaMeska5 <stameska40@mail.ru>
2025-07-05 12:23:20 +0200

    edit views.py

--- eshop/main/views.py
+++ eshop/main/views.py
@@ -16,7 +16,7 @@ def product_list(request, category_slug=None):
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
-    return render(request, 'main/product/detail.html',
+    return render(request, 'main/index/index.html',
                   {'category':category,
                    'categories':categories,
                    'products':products})
