7e0e65d62b64f767b1654d82b79148ff7b86f76d
StaMeska5 <stameska40@mail.ru>
2025-07-06 17:39:18 +0200

    123
    123

--- eshop/main/urls.py
+++ eshop/main/urls.py
@@ -7,8 +7,8 @@ app_name = 'main'
 urlpatterns = [
      path('', views.index_list, name='index_list'),
      path('shop/', views.product_list, name='product_list'),
-     path('shop/<slug:slug>/', views.product_details, name='product_detail'),
+     path('shop/men/', views.men_list, name='men_list'),
+     path('shop/women/', views.women_list, name='women_list'),
+     # path('shop/<slug:slug>/', views.product_details, name='product_detail'),
      path('shop/category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-     path('shop/men/<slug:slug>/', views.men_list, name='men_list'),
-     path('shop/women/<slug:slug>/', views.women_list, name='women_list'),
 ]
