d46459c02e7afa826138b1827ea6027af0cd3ad0
karina146 <karina14065@gmail.com>
2025-07-06 22:38:53 +0200

    .

--- eshop/main/views.py
+++ eshop/main/views.py
@@ -33,20 +33,20 @@ def men_list(request, slug=None):
                    'products':products,
                    'slug_url': slug})
 
-def women_list(request, slug=None):
+def women_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True, gender='female')
     
-    if slug:
-        category = get_object_or_404(Category, slug=slug)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=category, gender='female')
     
     return render(request, 'main/product/list_women.html',
                   {'category':category,
                    'categories':categories,
                    'products':products,
-                   'slug_url': slug})
+                   'slug_url': category_slug})
 
 def product_details(request, slug):
     product = get_object_or_404(Product, slug=slug, available=True)
