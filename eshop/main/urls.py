469f1dfd390dad3a0b3522b2276ce31f6b9b6fee
StaMeska5 <stameska40@mail.ru>
2025-07-08 16:31:14 +0200

    русификатор

--- eshop/main/urls.py
+++ eshop/main/urls.py
@@ -6,10 +6,10 @@ app_name = 'main'
 
 urlpatterns = [
      path('', views.index_list, name='index_list'),
-     path('shop/men/category/<slug:category_slug>/', views.product_list, {'gender': 'male'}, name='product_list_men_by_category'),
-     path('shop/women/category/<slug:category_slug>/', views.product_list, {'gender': 'female'}, name='product_list_women_by_category'),
-     path('shop/men/', views.product_list, {'gender': 'male'}, name='product_list_men'),
-     path('shop/women/', views.product_list, {'gender': 'female'}, name='product_list_women'),
+     path('shop/men/category/<slug:category_slug>/', views.product_list, {'gender': 'Мужской'}, name='product_list_men_by_category'),
+     path('shop/women/category/<slug:category_slug>/', views.product_list, {'gender': 'Женский'}, name='product_list_women_by_category'),
+     path('shop/men/', views.product_list, {'gender': 'Мужской'}, name='product_list_men'),
+     path('shop/women/', views.product_list, {'gender': 'Женский'}, name='product_list_women'),
      path('shop/category/<slug:category_slug>/', views.product_list, name='product_list_by_category'),
      path('shop/<slug:slug>/', views.product_details, name='product_detail'),
      path('shop/', views.product_list, name='product_list'),
