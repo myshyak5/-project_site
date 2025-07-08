f2c98d5d9d095fed74365906669085db167c8f1e
karina146 <karina14065@gmail.com>
2025-07-08 21:52:33 +0200

    .

--- eshop/cart/views.py
+++ eshop/cart/views.py
@@ -22,11 +22,11 @@ def cart_add (request, product_id):
 @require_POST
 def cart_remove(request, product_id):
     cart = Cart(request)
-    product = get_list_or_404(Product, id=product_id)
+    product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
 
 
 def cart_detail(request):
     cart = Cart(request)
-    return redirect(request, 'cart/detail.html', {'cart':cart})
+    return render(request, 'cart/detail.html', {'cart':cart})
