742813ea548af78511ef38fbee1c077177a56919
karina146 <karina14065@gmail.com>
2025-07-08 21:50:38 +0200

    object

--- eshop/cart/views.py
+++ eshop/cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from main.models import Product
 from .cart import Cart
@@ -8,7 +8,7 @@ from .forms import CartAddProductForm
 @require_POST
 def cart_add (request, product_id):
     cart = Cart(request)
-    product = get_list_or_404(Product, id=product_id)
+    product = get_object_or_404(Product, id=product_id)
     form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
